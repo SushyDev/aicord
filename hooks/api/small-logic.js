78bc0533efe3256f93741e47d57f7ff642878afa
Sushy <43114803+SushyDev@users.noreply.github.com>
2021-11-28 23:55:34 +0100

    EPIC

--- hooks/api/small-logic.js
+++ hooks/api/small-logic.js
@@ -2,16 +2,8 @@ module.exports = async (req, res) => {
     console.log(req.body);
 
     if (req.body.queryResult.intent.displayName === 'Default time') {
-        console.log('IS TIME');
         res.send({
-            fulfillment_messages: [
-                {
-                    text: {
-                        text: [`The time is ${new Date().toUTCString()}`],
-                    },
-                },
-            ],
-            fulfillmentText: 'THE TIME IS NOTHING',
+            fulfillmentText: `The time is ${new Date().toUTCString()}`,
         });
     }
 };
