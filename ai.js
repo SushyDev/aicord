1bb08efd46182f5c3748d4b804404ded5480e328
Sushy <43114803+SushyDev@users.noreply.github.com>
2021-11-28 20:35:42 +0100

    uuid

--- ai.js
+++ ai.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 const dialogflow = require('@google-cloud/dialogflow');
+const uuid = require('uuid');
 require('dotenv').config();
 client.login(process.env.TOKEN);
 client.on('ready', () => {
