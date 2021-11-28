da927da258cf8c7901307fd703ed33afa2d27105
Sushy <43114803+SushyDev@users.noreply.github.com>
2021-11-28 23:35:54 +0100

    a

---
+++ hooks/api/small-logic.js
@@ -0,0 +1,8 @@
+module.exports = async (req, res) => {
+    // this function will be launched when the API is called.
+    try {
+        res.send(await retrieveLyrics()); // send the lyrics
+    } catch (err) {
+        res.send(err); // send the thrown error
+    }
+};
