5e67fdee90106ed47c339de2be11abe0a81d4425
Sushy <43114803+SushyDev@users.noreply.github.com>
2021-11-29 00:50:46 +0100

    e

--- ai.js
+++ ai.js
@@ -74,7 +74,7 @@ client.on('ready', () => {
 
             if (!response || response.intent.isFallback || !response.fulfillmentText) return;
 
-            console.log(`\n New: ${date.toUTCString()}\n Message: ${message.content}\n   Guild: ${message.guild.name}\n   Channel: ${message.channel.name}\n   Reply: ${response.fulfillmentText}`);
+            console.log(`\n New: ${date.toUTCString()}\n  Message: ${message.content}\n   Guild: ${message.guild.name}\n   Channel: ${message.channel.name}\n   Reply: ${response.fulfillmentText}`);
             if (response.intent) {
                 console.log(`  Intent: ${response.intent.displayName}`);
             } else {
