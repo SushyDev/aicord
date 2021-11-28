4a2da5ce956b8e1b27346b3c7e4ab7d8e8fe22ea
Sushy <43114803+SushyDev@users.noreply.github.com>
2021-11-28 20:44:28 +0100

    text

--- ai.js
+++ ai.js
@@ -4,6 +4,43 @@ const fs = require('fs');
 const dialogflow = require('@google-cloud/dialogflow');
 const uuid = require('uuid');
 require('dotenv').config();
+
+async function runSample(projectId = process.env.PROJECT_ID) {
+    // A unique identifier for the given session
+    const sessionId = uuid.v4();
+
+    // Create a new session
+    const sessionClient = new dialogflow.SessionsClient();
+    const sessionPath = sessionClient.projectAgentSessionPath(projectId, sessionId);
+
+    // The text query request.
+    const request = {
+        session: sessionPath,
+        queryInput: {
+            text: {
+                // The query to send to the dialogflow agent
+                text: 'hello',
+                // The language used by the client (en-US)
+                languageCode: 'en-US',
+            },
+        },
+    };
+
+    // Send request and log result
+    const responses = await sessionClient.detectIntent(request);
+    console.log('Detected intent');
+    const result = responses[0].queryResult;
+    console.log(`  Query: ${result.queryText}`);
+    console.log(`  Response: ${result.fulfillmentText}`);
+    if (result.intent) {
+        console.log(`  Intent: ${result.intent.displayName}`);
+    } else {
+        console.log('  No intent matched.');
+    }
+}
+
+runSample();
+
 client.login(process.env.TOKEN);
 client.on('ready', () => {
     {
@@ -120,7 +157,7 @@ client.on('ready', () => {
 
                 message.channel.send(result);
             } catch (e) {
-                console.log(e)
+                console.log(e);
             }
 
             if (message.content.toLowerCase().includes('voice')) {
